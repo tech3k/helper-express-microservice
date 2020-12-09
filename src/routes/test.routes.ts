2b361f9be5e3bb49921c19e31ecf101659a8059f
Simon Skinner <vultuk28@gmail.com>
2020-12-09 16:35:29 +0000

    Pushing Deployment

--- src/routes/test.routes.ts
+++ src/routes/test.routes.ts
@@ -1,7 +1,15 @@
-import { Application } from 'express';
+import { Application, Request, Response } from 'express';
+
 import asyncMethod from '../shared/async-method';
 import test from '../app/testMethod';
 
 export default (app: Application): void => {
   app.get('/test', asyncMethod(test));
+
+  app.get(
+    '/test2',
+    asyncMethod(async (req: Request, res: Response) => {
+      res.json({ success: 'test2' }).end();
+    })
+  );
 };
